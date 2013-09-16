35136b4e07714d084171d35bedea2a98ba3c8bcb
Matthew Endsley <mendsley@gmail.com>
2013-09-15 20:15:33 -0700

    Update recast to use sunmath instead of CRT

--- Detour/Include/DetourMath.h
+++ Detour/Include/DetourMath.h
@@ -8,14 +8,26 @@ Members in this module are wrappers around the standard math library
 
 */
 
-#include <math.h>
+#if 0
+#	include <math.h>
 
-#define dtMathFabs(x) fabs(x)
-#define dtMathSqrtf(x) sqrtf(x)
-#define dtMathFloorf(x) floorf(x)
-#define dtMathCeilf(x) ceilf(x)
-#define dtMathCosf(x) cosf(x)
-#define dtMathSinf(x) sinf(x)
-#define dtMathAtan2f(y, x) atan2f(y, x)
+#	define dtMathFabs(x) fabs(x)
+#	define dtMathSqrtf(x) sqrtf(x)
+#	define dtMathFloorf(x) floorf(x)
+#	define dtMathCeilf(x) ceilf(x)
+#	define dtMathCosf(x) cosf(x)
+#	define dtMathSinf(x) sinf(x)
+#	define dtMathAtan2f(y, x) atan2f(y, x)
+#else
+#	include <sunmath.h>
+
+#	define dtMathFabs(x) sunmath::fabs(x)
+#	define dtMathSqrtf(x) sunmath::sqrtf(x)
+#	define dtMathFloorf(x) sunmath::floorf(x)
+#	define dtMathCeilf(x) sunmath::ceilf(x)
+#	define dtMathCosf(x) sunmath::cosf(x)
+#	define dtMathSinf(x) sunmath::sinf(x)
+#	define dtMathAtan2f(y, x) sunmath::atan2f(y, x)
+#endif
 
 #endif
